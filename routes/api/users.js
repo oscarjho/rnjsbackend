cf9db323545e867829b50bb6766418ac68f3a2e8
Oscar <oscarjavierho@gmail.com>
2019-12-27 11:06:55 -0430

    Added User Model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+// Load User model
+const User = require('../../models/User');
+
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
+
+// @route POST api/users/register
+// @desc Register user
+// @access Public
+router.post(
+  '/register',
+  (req,res) => {
+    console.log(req);
+  }
+);
 
 module.exports = router;
 
